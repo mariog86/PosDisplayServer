0f58644ad93e7da0acbcf5f8cc30b559c820ac7f
mariog86 <mariog86@gmail.com>
2015-11-17 19:54:17 +0100

    Initialize Solution.

---
+++ Pos.DisplayServer/Server.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FractionLibrary
+{
+    [TestFixture]
+    public class FractionTest
+    {
+        [Test]
+        public void FirstTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
