3e48c45464d5de05c45b0ed20db5cbac00e6aefa
mariog86 <mariog86@gmail.com>
2015-11-17 20:06:10 +0100

    Added Gui and AcceptanceTest project.

--- Pos.DisplayServer.Test/ServerTest.cs
+++ Pos.DisplayServer.Test/ServerTest.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace FractionLibrary.Test
 {
-    public class Class1
+    public class DisplayServerAcceptanceTest
     {
+        public void GivenClientAvailable_WhenSendingMessage_ThenDisplayed()
+        {
+            
+        }
     }
 }
